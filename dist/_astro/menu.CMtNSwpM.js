54a78a5c105631c79ecc33872e2762970d8c4709
Olivier Bossel <olivier.bossel@gmail.com>
2025-09-23 16:57:14 +0200

    deploy

---
+++ dist/_astro/menu.CMtNSwpM.js
@@ -0,0 +1,1 @@
+import{M as f}from"./menu.DDPA_rqF.js";import"./index.8ze02sE4.js";import"./codeTabs.B4dHvyrN.js";import"./runtime-dom.esm-bundler.CYfEf4Zo.js";import"./runtime-core.esm-bundler.BhZ-w4vx.js";import"./code.tRzSr69v.js";export{f as default};
